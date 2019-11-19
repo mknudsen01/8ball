d4a8985968bf8a32a8adaa40f9a56c0c383b9c6c
Matthew Knudsen <mknudsen01@gmail.com>
2019-11-19 12:02:49 -0800

    Add eight ball with no animations

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+const responses = [
+  "Only if you kiss your boyfriend",
+  "You? Seriously? No. Just, no",
+  "Come on, you're embarrassing yourself",
+  "Actually, yeah",
+  "YAS KWEEN",
+  "Ehhhhhh don't plan on it",
+  "Only if you become a completely different person",
+  "Yeah, sure. And pigs are flying now, too, right?"
+]
+
+const getRandomIndex = () => Math.floor(Math.random() * responses.length)
+
 function App() {
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div className="ball__outer">
+        <div className="ball__inner">
+          <div className="ball__triangle">
+            <div className="ball__content">
+              {responses[currentIndex]}
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="button" onClick={() => setCurrentIndex(getRandomIndex())}>TELL ME</div>
     </div>
   );
 }
